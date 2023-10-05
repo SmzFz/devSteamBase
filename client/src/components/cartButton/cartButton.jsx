72c377e9909f2e12ad3d442472186e9357eebaac
SmzFz <felipesalmazo123@gmail.com>
2023-10-05 11:25:30 -0300

    desenvolvendo cartbutton e cartmenu

--- client/src/components/cartButton/cartButton.jsx
+++ client/src/components/cartButton/cartButton.jsx
@@ -1,12 +1,15 @@
 import CartMenu from "../cartMenu/cartMenu";
 import styles from "./cartButton.module.css";
 import cart from "../../assets/cart.svg";
+import { useState } from "react";
 
 const CartButton = () => {
+    const [open, setOpen] = useState(true);
   return (
-    <div className={styles.cartButton}>
+    <div className={styles.cartButton} onClick={() => setOpen(!open)}>
       <img src={cart} alt="icone de carrinho de compra" width={"46"} />
-      <CartMenu />
+      
+      {open && <CartMenu />}
     </div>
   );
 };
--- client/src/components/cartMenu/cartMenu.jsx
+++ client/src/components/cartMenu/cartMenu.jsx
@@ -3,14 +3,10 @@ import styles from './cartMenu.module.css'
 
 const CartMenu = () => {
   return (
-    <div className={styles.menu}>
-      <div className={styles.options}>
+    <div className={styles.menu} onClick={(e) => e.stopPropagation()}>
+      <div className={styles.options}></div>
 
-      </div>
-
-      <div className={styles.priceLine}>
-
-      </div>
+      <div className={styles.priceLine}> </div>
 <Button fullwidth>Finalizar Compra</Button>
     </div>
   )
