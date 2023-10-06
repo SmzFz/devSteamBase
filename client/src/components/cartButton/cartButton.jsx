da4d47afeaf4a248a1f4ac79aacc8d602771e552
SmzFz <felipesalmazo123@gmail.com>
2023-10-06 13:47:02 -0300

    cardbutton finalizado

--- client/src/components/cartButton/cartButton.jsx
+++ client/src/components/cartButton/cartButton.jsx
@@ -6,11 +6,14 @@ import {cartState} from "../../atoms/cart"
 import {useRecoilValue} from "recoil";
 
 const CartButton = () => {
-    const [open, setOpen] = useState(true);
-    //const cart = useRecoilValue(cartState);
+    const [open, setOpen] = useState(false);
+    const cart = useRecoilValue(cartState);
+
   return (
     <div className={styles.cartButton} onClick={() => setOpen(!open)}>
       <img src={logo} alt="icone de carrinho de compra" width={"46"} />
+      {cart.length > 0 && <div className={styles.number}>{cart.length}</div>}
+
       {open && <CartMenu isOpen={open} onClose={() => setOpen(false)} />}
     </div>
   );
